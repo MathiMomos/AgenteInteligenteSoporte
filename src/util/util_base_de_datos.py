ce59f883dbcc73ab8e55ae57db1af60a6a22562b
MathiMomos <mathi.gamer.m@gmail.com>
2025-09-11 03:04:36 -0500

    AGENTE INTELIGENTE V1.1.1

---
+++ src/util/util_base_de_datos.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+
+from src.util import util_env as key
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(key.require("DATABASE_URL"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
